3751fda2beb863211bb2816fef4162ba0d0bdfce
Kushal425 <kushalsarkar457@gmail.com>
2025-05-07 00:59:46 +0530

    small

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© 2023 Your Company Name. All rights reserved.</p>
+        <p>© 2023 BidEstate. All rights reserved.</p>
         <ul className="social-media">
           <li><a href="#">Facebook</a></li>
           <li><a href="#">Twitter</a></li>
