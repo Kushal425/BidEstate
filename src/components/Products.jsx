46d4f8e6cc820454354135a73dcca4fcfd967a3e
Kushal425 <kushalsarkar457@gmail.com>
2025-05-07 00:36:25 +0530

    HomePageContent

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router";
+import "./Products.css";
 
 function Products() {
   return (
-    <div>Products</div>
-  )
+    <div>
+      <h1 className="title">On BidEstate You Can</h1>
+      <div className="Container">
+        <Link to="/buy" className="noline">
+          <div className="Buy">
+            <h1>Buy</h1>
+            <p>Bid on your dream property</p>
+          </div>
+        </Link>
+        <Link to="sell" className="noline">
+          <div className="Sell">
+            <h1>Sell</h1>
+            <p>List your property to get your price</p>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
