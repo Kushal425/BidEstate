24cc03e4ee50fa67ded5ffb99966cc736920c0c8
Kushal425 <kushalsarkar457@gmail.com>
2025-05-07 13:41:52 +0530

    LoginAuth0Functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,11 @@
 
 import React, { useEffect } from "react";
 import { Link } from "react-router";
+import { useAuth0 } from "@auth0/auth0-react";
 import "./Header.css";
 
 const Header = () => {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
   return (
     <header className="header">
       <Link to="/" className="noline">
@@ -25,7 +27,13 @@ const Header = () => {
         <Link to="/sell" className="noline"><li>Sell</li></Link>
         <Link to="/about" className="noline"><li>About</li></Link>
 
-        <button className="login-button">Log In</button>
+        {!isAuthenticated?<button className="login-button" onClick={() => loginWithRedirect()}>Log In</button>:<>
+        <div>
+          <img className="userProfile" src={user.picture} alt={user.name} />
+          <h2 className="userInfo">{user.name}</h2>
+        </div>
+        <button className="logout-button" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>Log Out</button>
+        </>}
       </nav>
     </header>
   );
